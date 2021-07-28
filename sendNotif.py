ce9c9b2dedd1288e35a5c66f5961cb0bcd825057
admin <admin@applebook.localdomain>
2021-07-28 16:59:13 -0700

    modify sendNotif again

--- sendNotif.py
+++ sendNotif.py
@@ -11,24 +11,11 @@ sent_from = gmail_user
 to = ['']
 subject = 'Device authenticated'
 
-DAD_MAC = ""
-MOM_MAC = ""
-VERA_MAC = ""
-EVAN_MAC = "0a:1d:85:3b:b1:ee"
-
 BLACKLIST = ["a4:4e:31:d3:5a:a8"]
 
 body = ""
 if argv[2] == "CONN":
-	if argv[1].lower() == DAD_MAC:
-		body = "Dad's iPhone has joined WiFi"
-	elif argv[1].lower() == MOM_MAC:
-		body = "Mom's iPhone has joined WiFi"
-	elif argv[1].lower() == VERA_MAC:
-		body = "Vera's iPhone has joined WiFi"
-	elif argv[1].lower() == EVAN_MAC:
-		body = "Welcome home Evan!"
-	elif argv[1].lower() in BLACKLIST:
+	if argv[1].lower() in BLACKLIST:
 		exit()
 	else:
 		try:
